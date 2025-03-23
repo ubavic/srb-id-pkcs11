7e39cff26ff40f5ba92c0a46d208b58277d47c4f
Nikola Ubavic <53820106+ubavic@users.noreply.github.com>
2025-03-23 14:12:44 +0100

    Parallel function management functions

--- src/pkcs11Parallel.cpp
+++ src/pkcs11Parallel.cpp
@@ -1,9 +1,9 @@
 #include "pkcs11Parallel.h"
 
 CK_DECLARE_FUNCTION(CK_RV, C_GetFunctionStatus)(CK_SESSION_HANDLE hSession) {
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	return CKR_FUNCTION_NOT_PARALLEL;
 }
 
 CK_DECLARE_FUNCTION(CK_RV, C_CancelFunction)(CK_SESSION_HANDLE hSession) {
-	return CKR_FUNCTION_NOT_SUPPORTED;
+	return CKR_FUNCTION_NOT_PARALLEL;
 }
